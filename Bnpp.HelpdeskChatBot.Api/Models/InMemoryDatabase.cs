eb10248de4885b663afec1c6132d5adbc1303588
chaibi <chaibi.med@hotmail.fr>
2018-09-25 12:28:36 +0100

    Sperate Repository and InMemory files

--- Bnpp.HelpdeskChatBot.Api/Models/InMemoryDatabase.cs
+++ Bnpp.HelpdeskChatBot.Api/Models/InMemoryDatabase.cs
@@ -1,22 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Extensions.Options;
 
 namespace Bnpp.HelpdeskChatBot.Api.Models
 {
-
-    public class Repository : IRepository
-    {
-        private InMemoryDatabase _db;
-        public Repository(IOptions<InMemoryDatabase> db)
-        {
-            this._db = db.Value;
-        }
-        public IList<string> GetApplications()
-        {
-            return _db.Applications;
-        }
-    }
     public class InMemoryDatabase 
     {
         public List<string> Applications { get; set; }
---
+++ Bnpp.HelpdeskChatBot.Api/Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+
+namespace Bnpp.HelpdeskChatBot.Api.Models
+{
+    public class Repository : IRepository
+    {
+        private InMemoryDatabase _db;
+        public Repository(IOptions<InMemoryDatabase> db)
+        {
+            this._db = db.Value;
+        }
+        public IList<string> GetApplications()
+        {
+            return _db.Applications;
+        }
+    }
+}
